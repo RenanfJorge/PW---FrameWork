4c7807b94db5424946139afa63a01aaefeb5ad2b
RenanfJorge <146784713+RenanfJorge@users.noreply.github.com>
2024-04-03 22:22:36 -0300

    PW - FreamWork
    Introduzindo JS

---
+++ css/js/Cadastrar.js
@@ -0,0 +1,38 @@
+document.querySelector("#botao-cadastrar")
+.addEventListener("click", () => {
+
+    const form = document.querySelector("form")
+
+    const tarefa = {
+        titulo : form.titulo.value,
+        descricao : form.descricao.value,
+        pontos : form.pontos.value
+
+    }
+
+
+    validar(tarefa)
+
+   
+
+})
+
+function validar(tarefa){
+    limparErros()
+    if(tarefa.titulo == ""){
+        document.querySelector("#titulo").classList.add ("is-error")
+        document.querySelector("#titulo-error").innerText = "o titulo é obrigatório"
+    }
+    if(tarefa.descricao == ""){
+        document.querySelector("#descricao-error").innerText = "A descrição é obrigatório"
+        document.querySelector("#descricao-error").classList.add ("is-error")
+    }
+
+}
+
+function limparErros(){
+        document.querySelector("#titulo").classList.remove ("is-error")
+        document.querySelector("#titulo-error").innerText = ""
+
+    
+}
